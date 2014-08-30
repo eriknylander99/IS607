c7e29050e505fa059e19d8ddfe1d84377bc6c5f8
eriknylander99 <eriknylander@gmail.com>
2014-08-29 22:19:45 -0400

    Week 1 Assignment

--- Week1Assignment.R
+++ Week1Assignment.R
@@ -1,10 +1,10 @@
-#### Question 1 ####
+#### Question 1 - R Version ####
 # R version 3.1.0 (2014-04-10) -- "Spring Dance"
 
-#### Question 2 ####
+#### Question 2 - PostgreSQL Version ####
 # PostgreSQL 9.3(x64)
 
-#### Question 3 ####
+#### Question 3 - Installing and Loading the DMwR package ####
 install.packages("DMwR")
 require(DMwR)
 nrow(sales) # There are 401,146 observations in the sales data
